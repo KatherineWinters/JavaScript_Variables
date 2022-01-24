81fb5254731d7a7f8ced380e0990cb58cd93f5cf
Katherine Winters <goble.katherine@gmail.com>
2022-01-24 13:16:44 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+console.log("Follow the steps in the README.md file to complete the exercises:\n==========\n");
+
+
 
 // Exercise 1
+const firstName = "Katherine";
+let lastName = "Winters";
+var age = 28;
 
 // Exercise 2
 
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
 // Exercise 3
+
+let city = "St. Charles";
+let passTime = "listening to podcasts";
+let myStory = 'My name is ${fullName}. I am from ${city} and I enjoy listing to ${passTime}';
+console.log(myStory);
